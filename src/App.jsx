6d1856fe2e2497d1df641515de611bc5f24a75b4
Pedro <38092988+Toriality@users.noreply.github.com>
2024-01-05 15:00:07 -0300

    deploy test

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/game" element={<Game />} />
